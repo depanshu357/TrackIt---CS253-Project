373bf78cbff933846707f1b6237b48688e229635
Depanshu <depanshusahu057@gmail.com>
2023-01-24 09:28:09 +0530

    made backend,singup,authentication and token

---
+++ backend/controllers/expenseController.js
@@ -0,0 +1,103 @@
+const Expense = require('../models/expenseModel')
+const mongoose = require('mongoose')
+
+// get all workouts
+const getExpenses = async (req, res) => {
+  const user_id = req.user._id
+
+  const expenses = await Expense.find({user_id}).sort({createdAt: -1})
+
+  res.status(200).json(expenses)
+}
+
+// get a single workout
+const getExpense = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout'})
+  }
+
+  const expense = await Expense.findById(id)
+
+  if (!expense) {
+    return res.status(404).json({error: 'No such workout'})
+  }
+  
+  res.status(200).json(expense)
+}
+
+
+// create new workout
+const createExpense = async (req, res) => {
+  const {Item, MoneySpent, Description} = req.body
+
+  let emptyFields = []
+
+  if(!Item) {
+    emptyFields.push('Item')
+  }
+  if(!MoneySpent) {
+    emptyFields.push('MoneySpent')
+  }
+  if(!Description) {
+    emptyFields.push('Description')
+  }
+  if(emptyFields.length > 0) {
+    return res.status(400).json({ error: 'Please fill in all the fields', emptyFields })
+  }
+
+  // add doc to db
+  try {
+    const user_id = req.user._id
+    const expense = await Expense.create({Item, MoneySpent, Description, user_id})
+    res.status(200).json(expense)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+// delete a workout
+const deleteExpense = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such Expenses'})
+  }
+
+  const expense = await Expense.findOneAndDelete({_id: id})
+
+  if (!expense) {
+    return res.status(400).json({error: 'No such expenses'})
+  }
+
+  res.status(200).json(expense)
+}
+
+// update a workout
+const updateExpense = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such expenses'})
+  }
+
+  const expense = await Workout.findOneAndUpdate({_id: id}, {
+    ...req.body
+  })
+
+  if (!expense) {
+    return res.status(400).json({error: 'No such expenses'})
+  }
+
+  res.status(200).json(expense)
+}
+
+
+module.exports = {
+    getExpenses,
+  getExpense,
+  createExpense,
+  deleteExpense,
+  updateExpense
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,42 @@
 // import { Routes } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import CurrentMonth from "./Components/CurrentMonth";
 import History from "./Components/History";
 import Profile from "./Components/Profile";
-import Login from "./Components/Login";
-import SignUp from "./Components/SignUp";
+// import Login from "./Components/Login";
+// import SignUp from "./Components/SignUp";
+import { useAuthContext } from "./hooks/useAuthContext";
 
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
-    <>
-      <div>
-        {/* <Navbar /> */}
-        <Router>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
           <Routes>
-            <Route path="/" element={<Navbar />}>
-              {/* <Route path="/" element={<Navbar />}></Route> */}
-              <Route path="/currentMonth" element={<CurrentMonth />}></Route>
-              <Route path="/history" element={<History />}></Route>
-              <Route path="/profile" element={<Profile />}></Route>
-            </Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/register" element={<SignUp />}></Route>
+            <Route 
+              path="/" 
+              element={user ? <Home /> : <Navigate to="/login" />} 
+            />
+            <Route 
+              path="/login" 
+              element={!user ? <Login /> : <Navigate to="/" />} 
+            />
+            <Route 
+              path="/signup" 
+              element={!user ? <Signup /> : <Navigate to="/" />} 
+            />
           </Routes>
-        </Router>
-      </div>
-    </>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
