b9ada7f1707d796faeb2d0c5b717a2a6d7a255cf
Depanshu <depanshusahu057@gmail.com>
2023-03-07 00:28:52 +0530

    ok

--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -13,6 +13,7 @@ const Profile = () => {
 
       Profile Page
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi quas beatae velit doloremque, nemo exercitationem sed veniam aliquam tenetur! Nesciunt deleniti officia consectetur voluptatibus voluptates beatae autem adipisci alias assumenda blanditiis! Veniam, quas voluptatum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic repellendus omnis pariatur eum corporis dolores iusto quaerat laudantium culpa distinctio, maxime quis sed beatae consequatur magni ab sapiente? Corporis sint alias eum sequi numquam.
       </h1>
     </div>
   )
