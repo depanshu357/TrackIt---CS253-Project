5cd6ef033c46c743798c17b8d2094ea9d6aebb86
Depanshu <depanshusahu057@gmail.com>
2023-03-07 14:26:29 +0530

    bug found

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,6 +13,7 @@ const loginUser = async (req, res) => {
     const user = await User.login(email, password)
     let userType = user.userType;
     let rollNo = user.rollNo;
+    let shopName = user.shopName;
 
     // create a token
     const token = createToken(user._id)
