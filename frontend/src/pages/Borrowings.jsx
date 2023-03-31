f1fe4cb809b6de622d0e3d598c4d974d4b00e103
aryanmaurya383 <aryanmaurya383@gmail.com>
2023-03-31 16:09:43 +0530

    Borrowings page updated

--- frontend/src/pages/Borrowings.jsx
+++ frontend/src/pages/Borrowings.jsx
@@ -12,16 +12,18 @@ import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
 import ExpenseDetails from "../Components/ExpenseDetails";
 import ExpenseForm from "../Components/ExpenseForm";
 import DuesDetailsForCustomer from "../Components/DuesDetailsForCustomer";
-
+import Calendar from 'react-calendar'
 import "./customer.css";
-import { kk } from "date-fns/locale"; 
+import { kk } from "date-fns/locale";
 import { pink, lightGreen, blue } from "@mui/material/colors";
 
 const Borrowings = () => {
   const { expense, dispatch } = useExpenseContext();
   const { user } = useAuthContext();
-  const [rollNo,setRollNo] = useState(null)
+  const [rollNo, setRollNo] = useState(null)
   const [borrows, setBorrows] = useState(null);
+  const [date, setDate] = useState(new Date());
+  const [hasData, setHasData] = useState(false);
   const { Dues, dispatch: dispatchd } = useDuesContext();
 
   useEffect(() => {
@@ -52,7 +54,7 @@ const Borrowings = () => {
       const json = await response.json();
 
       if (response.ok) {
-  
+
         // console.log(json[0])
         // setBorrows(json);
         // console.log(borrows[0].RollNo);
@@ -64,236 +66,126 @@ const Borrowings = () => {
       fetchDues();
     }
   }, [dispatchd, user]);
-  let month = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  let data_to_show;
-  let Food = 0;
-  let Health = 0;
-  let Shopping = 0;
-  let Others = 0;
-  var date = new Date();
-  var current_month = date.getMonth();
-  var todays_date = date.getDate();
-  var current_year = date.getFullYear();
-  var total_data = [];
-  var last_ten_days_name = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-  var last_ten_days_value = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  var last_ten_days_data = [];
-  var piechart_data = [];
-  var bar_graph_progress = 0;
-  var total_budget = 20000;
-  for (let i = 1; i <= 12; i++) {
-    let temp = 0;
-    for (let j = 0; expense && j < expense.length; j++) {
-      // console.log(expense[j].Date[5] + expense[j].Date[6], current_month);
-      if (expense[j].Date[5] + expense[j].Date[6] == current_month + 1) {
-        bar_graph_progress += expense[j].MoneySpent;
-      }
-      if (expense[j].Date[5] + expense[j].Date[6] == i) {
-        temp += expense[j].MoneySpent;
 
-        if (todays_date >= 11) {
-          if (
-            expense[j].Date.substring(0, 4) == current_year &&
-            expense[j].Date[5] + expense[j].Date[6] == current_month + 1
-          ) {
-            if (
-              Number(todays_date) - Number(expense[j].Date.substring(8, 10)) <=
-                10 &&
-              Number(todays_date) - Number(expense[j].Date.substring(8, 10)) > 0
-            ) {
-              last_ten_days_name[
-                10 -
-                  (Number(todays_date) -
-                    Number(expense[j].Date.substring(8, 10)))
-              ] = expense[j].Date.substring(0, 10);
-              last_ten_days_value[
-                10 -
-                  (Number(todays_date) -
-                    Number(expense[j].Date.substring(8, 10)))
-              ] += expense[j].MoneySpent;
-            }
-          }
-        } else {
-          if (
-            expense[j].Date.substring(0, 4) == current_year &&
-            expense[j].Date[5] + expense[j].Date[6] == current_month + 1
-          ) {
-            if (
-              Number(todays_date) - Number(expense[j].Date.substring(8, 10)) <=
-                10 &&
-              Number(todays_date) - Number(expense[j].Date.substring(8, 10)) > 0
-            ) {
-              last_ten_days_name[
-                10 -
-                  (Number(todays_date) -
-                    Number(expense[j].Date.substring(8, 10)))
-              ] = expense[j].Date.substring(0, 10);
-              last_ten_days_value[
-                10 -
-                  (Number(todays_date) -
-                    Number(expense[j].Date.substring(8, 10)))
-              ] += expense[j].MoneySpent;
-            }
-          } else if (
-            expense[j].Date.substring(0, 4) == current_year &&
-            expense[j].Date[5] + expense[j].Date[6] == current_month
-          ) {
-            if (
-              current_month == 2 ||
-              current_month == 4 ||
-              current_month == 6 ||
-              current_month == 7 ||
-              current_month == 9 ||
-              current_month == 11
-            ) {
-              if (current_month == 2) {
-                if (
-                  Number(expense[j].Date.substring(8, 10)) >
-                  28 - (10 - todays_date + 1)
-                ) {
-                  last_ten_days_name[
-                    28 - Number(expense[j].Date.substring(8, 10)) - 2
-                  ] = expense[j].Date.substring(0, 10);
-                  last_ten_days_value[
-                    28 - Number(expense[j].Date.substring(8, 10)) - 2
-                  ] += expense[j].MoneySpent;
-                }
-              } else {
-                if (
-                  Number(expense[j].Date.substring(8, 10)) >
-                  30 - (10 - todays_date + 1)
-                ) {
-                  last_ten_days_name[
-                    30 - Number(expense[j].Date.substring(8, 10)) - 2
-                  ] = expense[j].Date.substring(0, 10);
-                  last_ten_days_value[
-                    30 - Number(expense[j].Date.substring(8, 10)) - 2
-                  ] += expense[j].MoneySpent;
-                }
-              }
-            } else {
-              if (
-                Number(expense[j].Date.substring(8, 10)) >
-                31 - (10 - todays_date + 1)
-              ) {
-                last_ten_days_name[
-                  31 - Number(expense[j].Date.substring(8, 10)) - 2
-                ] = expense[j].Date.substring(0, 10);
-                last_ten_days_value[
-                  31 - Number(expense[j].Date.substring(8, 10)) - 2
-                ] += expense[j].MoneySpent;
-              }
-            }
-          }
-        }
-
-        if (i == current_month + 1) {
-          if (expense[j].Category == "Food") {
-            Food += expense[j].MoneySpent;
-          }
-          if (expense[j].Category == "Health") {
-            Health += expense[j].MoneySpent;
-          }
-          if (expense[j].Category == "Shopping") {
-            Shopping += expense[j].MoneySpent;
-          }
-          if (expense[j].Category == "Others") {
-            Others += expense[j].MoneySpent;
-          }
-        }
-      }
-    }
-    total_data.push({ name: month[i - 1], value: temp });
-  }
-  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "black"];
-
-  for (let i = 0; i < 10; i++) {
-    last_ten_days_data.push({
-      name: last_ten_days_name[i],
-      value: last_ten_days_value[i],
-    });
-  }
-  piechart_data.push({ name: "Food", value: Food });
-  piechart_data.push({ name: "Health", value: Health });
-  piechart_data.push({ name: "Shopping", value: Shopping });
-  piechart_data.push({ name: "Other", value: Others });
-  piechart_data.push({
-    name: "remaining",
-    value: user.budget?user.budget:total_budget - Food - Health - Shopping,
-  });
   const date_dep = new Date();
   const todays_month = date_dep.getMonth();
+  const onChange = date => {
+    setDate(date)
+  }
+  var d = date;
+  var month = '' + (d.getMonth() + 1);
+  //var date1 = '' + d.getDate();
+  var year = '' + d.getFullYear();
+  if (month.length < 2) {
+    month = '0' + month;
+  }
+  // if (date1.length < 2) {
+  //   date1 = '0' + date1;
+  // }
 
-  const RADIAN = Math.PI / 180;
-  const renderCustomizedLabel = ({
-    cx,
-    cy,
-    midAngle,
-    innerRadius,
-    outerRadius,
-    percent,
-    index,
-  }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  const date2 = year + "-" + month;
+  console.log(date2);
+  //console.log(typeof date2);
 
-    return (
-      <text
-        x={x}
-        y={y}
-        fill="white"
-        textAnchor={x > cx ? "start" : "end"}
-        dominantBaseline="central"
-      >
-        {`${(percent * 100).toFixed(0)}%`}
-      </text>
-    );
+  var borrowings_data = [];
+  for (let i = 0; i < Dues.length; i++) {
+    if (Dues[i].RollNo == user.rollNo) {
+      borrowings_data.push(Dues[i]);
+    }
+  }
+  console.log(user.rollNo);
+  console.log(Dues);
+  const namesdate = borrowings_data.filter(function (el) {
+    // console.log(el.Date, date2, el.Date == date2);
+    return el.Date.substring(0, 7) == date2;
+  });
+  if (namesdate.length != 0 && !hasData) {
+    setHasData(true);
+  }
+  if (namesdate.length == 0 && hasData) {
+    setHasData(false);
+  }
+
+  const renderListOfUserNames = (namesdate) => {
+
+    return namesdate.map((name) => (
+
+      <div className="hi1">
+
+        <div className="listdetails1">
+          <span className="listname1-span">
+            <div className="listname1">{name.shopName}</div>
+          </span>
+          <span className="listmoney1-span">
+            <div className="listmoney1">₹{name.Amount}</div>
+          </span>
+          <span className="listdate1-span">
+            <div className="listdate1">{name.Date.substring(0, 10)}</div>
+          </span>
+          <span className="listpaid1-span">
+            {/* <div className="listdate1" style={{ color: name.Paid ? 'green' : 'red' }} >{name.Paid ? 'Paid' : 'Unpaid'}</div> */}
+            {name.Paid ? <span class="inline-flex items-center bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
+              <span class="w-2 h-2 mr-1 bg-green-500 rounded-full"></span>
+              Paid
+            </span> : <span class="inline-flex items-center bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">
+              <span class="w-2 h-2 mr-1 bg-red-500 rounded-full"></span>
+              Unpaid
+            </span>}
+          </span>
+          {/* <button className="delete-btn" onClick={handleClick} >delete</button> */}
+
+
+          {/* <a
+            className="btn btn-custom listcollapsebutton1"
+            data-bs-toggle="collapse"
+            href={`#collapseExample${name._id}`}
+            role="button"
+            aria-expanded="false"
+            aria-controls="collapseExample"
+          >
+            <span className="textv">
+              <KeyboardArrowDownIcon />
+            </span>
+          </a> */}
+
+        </div>
+        <div className=" listdesc1"><span style={{ color: 'gray' }}>Description: </span>{name.Description}</div>
+
+        {/* <div className="collapse listdescription1" id={`collapseExample${name._id}`}>
+          <div className=" listdesc1">{name.Item}</div>
+        </div> */}
+      </div>
+
+    ));
   };
 
   return (
-      <div className="home-customer-bot-expense">
-        
-        <div className="dues-history">
-          <div className="expense-history-heading-cover">
-            <div
-              className="expense-details-heading"
-              style={{ fontSize: "25px", fontWeight: "bolder" }}
-            >
-              Recent Borrowings
-            </div>
-            <div className="expense-details-heading">
-              <span>Item</span>
-              <span>Amount</span>
-              <span>Payement Status</span>{" "}
-            </div>
-          </div>
-          {Dues &&
-            Dues.map((borrow) => {
-              // console.log(borrow.RollNo)
-                if(user.rollNo == borrow.RollNo)
-                return (
-                  <div>
-                  <DuesDetailsForCustomer key={borrow._id} due={borrow} />
+    <>
+
+      <div className="everything-b">
+        <div className="calendaryear-comp">
+          <Calendar onChange={onChange} value={date} maxDetail='year' minDetail="year" defaultView="year" />
+        </div>
+        <div className="right">
+          <h1 className="monthlyborrowings">Monthly Borrowings</h1>
+          <div className="borrowings-comp">
+            <ul>{hasData ? renderListOfUserNames(namesdate) :
+              <div className="hi1">
+
+                <div className="listdetails1">
+                  <span className="listname1-span">
+                    <div className="listname1">No Entries</div>
+                  </span>
                 </div>
-              );
-            })}
+              </div>
+            }</ul>
+          </div>
+
         </div>
       </div>
-    
+
+
+    </>
   );
 };
 
